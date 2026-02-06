5bdc72beda5045989925e518181be67902e94f03
PensiveEagle <halljf96@gmail.com>
2026-02-06 11:07:30 +0000

    Initial main commit
    Set up api key retreival

---
+++ main.py
@@ -0,0 +1,9 @@
+# ----- Import packages ----- #
+from dotenv import load_dotenv
+import os
+
+# ----- Get env variables ----- #
+load_dotenv()
+
+todoist_api_key = os.getenv( "TODOIST_API_KEY" )
+gemini_api_key = os.getenv( "GEMINI_API_KEY" )
