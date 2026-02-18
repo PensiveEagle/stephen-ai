3e4fa0fbe49ef063adae0d2e460f97f8afb55c24
PensiveEagle <halljf96@gmail.com>
2026-02-18 14:05:04 +0000

    Allowed the agent to loop messages
    Added conversational loop to the programme
    and allowed the agent to track the history

--- main.py
+++ main.py
@@ -7,20 +7,28 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.tools import tool
 from langchain.agents import create_openai_tools_agent, AgentExecutor
+from todoist_api_python.api import TodoistAPI
+from datetime import datetime
 
 # ----- Get env variables ----- #
 load_dotenv()
 
-todoist_api_key = os.getenv( "TODOIST_API_KEY" )
+todoist_api_key = str( os.getenv( "TODOIST_API_KEY" ) )
 gemini_api_key = os.getenv( "GEMINI_API_KEY" )
+
+# ----- Initialise Todoist API ----- #
+todoist = TodoistAPI( todoist_api_key )
 
 # ----- Define Functions ----- #
 @tool
-def add_task(task):
+def add_task(task, task_description = None):
     '''
     Adds a new task to the users task list. Use this when the user wants to add or create a task
     '''
     print( f"Adding {task}!" )
+    
+    todoist.add_task( content = task, description = task_description )
+    
     print( f"{task} added" )
 
 # ----- Langchain ----- #
@@ -32,17 +40,26 @@ llm = ChatGoogleGenerativeAI(
 )
 
 system_prompt = "You are a helpful assistant. You will help the user at tasks"
-user_input = "Add a new task to buy milk"
 
 prompt = ChatPromptTemplate([
     ( "system", system_prompt ),
-    ( "user", user_input ),
-    MessagesPlaceholder("agent_scratchpad")
+    MessagesPlaceholder( "history" ),
+    ( "user", "{input}" ),
+    MessagesPlaceholder( "agent_scratchpad" )
 ])
 
 # chain = prompt | llm | StrOutputParser()
 agent = create_openai_tools_agent( llm, tools, prompt )
 agent_exec = AgentExecutor( agent = agent, tools = tools, verbose = True )
 
-response = agent_exec.invoke( { "input" : user_input } )
-print( response )
+history = []
+while True:
+    user_input = input( "You: " )
+    
+    if user_input == "stop":
+        break
+    
+    response = agent_exec.invoke( { "input": user_input, "history": history } )
+    print( response["output"] )
+    history.append( HumanMessage( content = user_input ) )
+    history.append( AIMessage( content = response["output"] ) )
