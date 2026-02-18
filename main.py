fb2eea6f1410adde44f5791c90536f9b27f83a01
PensiveEagle <halljf96@gmail.com>
2026-02-18 12:26:32 +0000

    Enabled passing of params to task tool

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from dotenv import load_dotenv
 import os
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.tools import tool
+from langchain.agents import create_openai_tools_agent, AgentExecutor
 
 # ----- Get env variables ----- #
 load_dotenv()
@@ -12,7 +14,17 @@ load_dotenv()
 todoist_api_key = os.getenv( "TODOIST_API_KEY" )
 gemini_api_key = os.getenv( "GEMINI_API_KEY" )
 
+# ----- Define Functions ----- #
+@tool
+def add_task(task):
+    '''
+    Adds a new task to the users task list. Use this when the user wants to add or create a task
+    '''
+    print( f"Adding {task}!" )
+    print( f"{task} added" )
+
 # ----- Langchain ----- #
+tools = [ add_task ]
 llm = ChatGoogleGenerativeAI(
     model = "gemini-2.5-flash",
     google_api_key = gemini_api_key,
@@ -20,14 +32,17 @@ llm = ChatGoogleGenerativeAI(
 )
 
 system_prompt = "You are a helpful assistant. You will help the user at tasks"
-user_input = "Tell me how best to manage my time"
+user_input = "Add a new task to buy milk"
 
 prompt = ChatPromptTemplate([
     ( "system", system_prompt ),
-    ( "user", user_input )
+    ( "user", user_input ),
+    MessagesPlaceholder("agent_scratchpad")
 ])
 
-chain = prompt | llm | StrOutputParser()
+# chain = prompt | llm | StrOutputParser()
+agent = create_openai_tools_agent( llm, tools, prompt )
+agent_exec = AgentExecutor( agent = agent, tools = tools, verbose = True )
 
-response = chain.invoke( { "input" : "user_input" })
+response = agent_exec.invoke( { "input" : user_input } )
 print( response )
